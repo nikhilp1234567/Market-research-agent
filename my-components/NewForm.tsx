24072c3e8905436d88776bc2896775981d840a65
nikhilp1234567 <nikhil_parmar@hotmail.com>
2025-02-26 11:58:39 +0000

    add extra ui fixes to boxes

--- my-components/NewForm.tsx
+++ my-components/NewForm.tsx
@@ -16,12 +16,9 @@ export default function NewForm() {
           style={{ backgroundColor: "#070F2B", borderRadius: "0.75rem" }}
           className='p-32 justify-center flex flex-1 gap-6 flex-col w-full h-full'>
           <h1 className='flex w-fit text-4xl'>Market Research Campaign Name:</h1>
-          <textarea
-            {...register("name")}
-            placeholder='Enter Name Here...'
-            autoFocus
-            className=' outline-1 p-3 !outline-white bg-transparent rounded-xl w-[50%] h-12'
-            id=''></textarea>
+          <div className='flex justify-center items-center p-2 !border-solid !border-white border-[3px] w-[70%] rounded-xl'>
+            <textarea {...register("name")} placeholder='Enter Name Here...' autoFocus className='w-full outline-none bg-transparent' id=''></textarea>
+          </div>
           <button
             onClick={() => {
               handleSubmit((data) => {
@@ -41,12 +38,15 @@ export default function NewForm() {
           style={{ backgroundColor: "#070F2B", borderRadius: "0.75rem" }}
           className='p-32 justify-center flex flex-1 gap-6 flex-col w-full h-full'>
           <h1 className='flex w-fit text-4xl'>Market Research Campaign Goal:</h1>
-          <select {...register("goal")} className=' outline-1 p-3 !outline-white bg-transparent rounded-xl w-[50%] h-12'>
-            <option value=' '>Select Campaign Goal</option>
-            <option value='validatePMF'>Validate product-market fit</option>
-            <option value='digitalAd'>Test a digital ad</option>
-            <option value='customerInsights'>Gather customer insights</option>
-          </select>
+          <div className='flex justify-center items-center p-2 !border-solid !border-white border-[3px] w-[70%] rounded-xl'>
+            <select {...register("goal")} className='outline-none bg-transparent rounded-xl w-full'>
+              <option value=' '>Select Campaign Goal</option>
+              <option value='validatePMF'>Validate product-market fit</option>
+              <option value='digitalAd'>Test a digital ad</option>
+              <option value='customerInsights'>Gather customer insights</option>
+            </select>
+          </div>
+
           <button
             onClick={() => {
               handleSubmit((data) => {
@@ -74,8 +74,8 @@ export default function NewForm() {
           </div>
           <div>
             <label htmlFor=''>Gender:</label>
-            <div className='mt-3 !border-solid !border-white border-[3px] w-[70%] rounded-xl'>
-              <select {...register("gender")} className='bg-transparent w-full h-12 outline-none'>
+            <div className='flex justify-center items-center p-2 !border-solid !border-white border-[3px] w-[70%] rounded-xl'>
+              <select {...register("gender")} className='bg-transparent w-full outline-none'>
                 <option value=' '>Select Gender</option>
                 <option value='Male'>Male</option>
                 <option value='Female'>Female</option>
@@ -85,15 +85,24 @@ export default function NewForm() {
           </div>
           <div>
             <label htmlFor=''>Interests:</label>
-            <div className='mt-3 p-1 pr-4 !border-solid !border-white border-[3px] w-[70%] rounded-xl'>
+            <div className='flex justify-center items-center p-2 !border-solid !border-white border-[3px] w-[70%] rounded-xl'>
               <textarea
-                className='m-2 w-full h-full outline-none bg-transparent'
+                className='w-full h-full outline-none bg-transparent'
                 placeholder='Enter interests as a comma separated list (eg backpacking, web-app development)'
                 name=''
                 id=''></textarea>
             </div>
           </div>
-          {/* interests */}
+          <div>
+            <label htmlFor=''>Locations:</label>
+            <div className='flex justify-center items-center p-2 !border-solid !border-white border-[3px] w-[70%] rounded-xl'>
+              <textarea
+                className='w-full h-full outline-none bg-transparent'
+                placeholder='Enter locations as a comma separated list. Cities, regions, countries and areas accepted.'
+                name=''
+                id=''></textarea>
+            </div>
+          </div>
           {/* employment */}{" "}
           <button
             onClick={() => {
