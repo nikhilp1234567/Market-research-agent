c7a63fe2c05835b5998b33a992e6e70c61216c98
nikhilp1234567 <nikhil_parmar@hotmail.com>
2025-02-26 11:37:05 +0000

    continue with ui to half of demographics

--- my-components/NewForm.tsx
+++ my-components/NewForm.tsx
@@ -2,6 +2,7 @@
 import React from "react";
 import { useForm } from "react-hook-form";
 import { useState } from "react";
+import AgeSlider from "./AgeSlider";
 
 export default function NewForm() {
   const [currentStep, setCurrentStep] = useState(1);
@@ -21,6 +22,79 @@ export default function NewForm() {
             autoFocus
             className=' outline-1 p-3 !outline-white bg-transparent rounded-xl w-[50%] h-12'
             id=''></textarea>
+          <button
+            onClick={() => {
+              handleSubmit((data) => {
+                console.log(data || "nothing");
+                setCurrentStep(currentStep + 1);
+              })();
+            }}
+            className='bg-slate-700 px-6 transition-all hover:bg-white hover:text-black py-3 rounded-md w-fit'>
+            Next
+          </button>
+        </div>
+      );
+    case 2:
+      return (
+        <div
+          id='blue-background-holder'
+          style={{ backgroundColor: "#070F2B", borderRadius: "0.75rem" }}
+          className='p-32 justify-center flex flex-1 gap-6 flex-col w-full h-full'>
+          <h1 className='flex w-fit text-4xl'>Market Research Campaign Goal:</h1>
+          <select {...register("goal")} className=' outline-1 p-3 !outline-white bg-transparent rounded-xl w-[50%] h-12'>
+            <option value=' '>Select Campaign Goal</option>
+            <option value='validatePMF'>Validate product-market fit</option>
+            <option value='digitalAd'>Test a digital ad</option>
+            <option value='customerInsights'>Gather customer insights</option>
+          </select>
+          <button
+            onClick={() => {
+              handleSubmit((data) => {
+                console.log(data);
+                setCurrentStep(currentStep + 1);
+              })();
+            }}
+            className='bg-slate-700 px-6 transition-all hover:bg-white hover:text-black py-3 rounded-md w-fit'>
+            Next
+          </button>
+        </div>
+      );
+    case 3:
+      return (
+        <div
+          id='blue-background-holder'
+          style={{ backgroundColor: "#070F2B", borderRadius: "0.75rem" }}
+          className='px-32 py-16 flex flex-1 gap-6 justify-between overflow-auto flex-col w-full max-h-full h-full'>
+          <h1 className='flex w-fit text-4xl'>Campaign Demographic Controls:</h1>
+          <div className='w-[70%]'>
+            <label htmlFor='age'>Age Range:</label>
+            <div className='mt-3'>
+              <AgeSlider />
+            </div>
+          </div>
+          <div>
+            <label htmlFor=''>Gender:</label>
+            <div className='mt-3 !border-solid !border-white border-[3px] w-[70%] rounded-xl'>
+              <select {...register("gender")} className='bg-transparent w-full h-12 outline-none'>
+                <option value=' '>Select Gender</option>
+                <option value='Male'>Male</option>
+                <option value='Female'>Female</option>
+                <option value='Male, Female'>Both</option>
+              </select>
+            </div>
+          </div>
+          <div>
+            <label htmlFor=''>Interests:</label>
+            <div className='mt-3 p-1 pr-4 !border-solid !border-white border-[3px] w-[70%] rounded-xl'>
+              <textarea
+                className='m-2 w-full h-full outline-none bg-transparent'
+                placeholder='Enter interests as a comma separated list (eg backpacking, web-app development)'
+                name=''
+                id=''></textarea>
+            </div>
+          </div>
+          {/* interests */}
+          {/* employment */}{" "}
           <button
             onClick={() => {
               handleSubmit((data) => {
