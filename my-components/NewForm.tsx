31edde361f2ca4a03634979857c0b75b56c00955
nikhilp1234567 <nikhil_parmar@hotmail.com>
2025-02-27 14:45:59 +0000

    finish the frontend without optimization and backend demographics

--- my-components/NewForm.tsx
+++ my-components/NewForm.tsx
@@ -3,6 +3,10 @@ import React from "react";
 import { useForm } from "react-hook-form";
 import { useState } from "react";
 import AgeSlider from "./AgeSlider";
+import Checkbox from "@mui/material/Checkbox";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import { blue } from "@mui/material/colors";
+import { dividerClasses } from "@mui/material";
 
 export default function NewForm() {
   const [currentStep, setCurrentStep] = useState(1);
@@ -84,9 +88,10 @@ export default function NewForm() {
             </div>
           </div>
           <div>
-            <label htmlFor=''>Interests:</label>
+            <label htmlFor=''>Interests and Behaviours:</label>
             <div className='flex justify-center items-center p-2 !border-solid !border-white border-[3px] w-[70%] rounded-xl'>
               <textarea
+                {...register("interests")}
                 className='w-full h-full outline-none bg-transparent'
                 placeholder='Enter interests as a comma separated list (eg backpacking, web-app development)'
                 name=''
@@ -97,13 +102,108 @@ export default function NewForm() {
             <label htmlFor=''>Locations:</label>
             <div className='flex justify-center items-center p-2 !border-solid !border-white border-[3px] w-[70%] rounded-xl'>
               <textarea
+                {...register("locations")}
                 className='w-full h-full outline-none bg-transparent'
                 placeholder='Enter locations as a comma separated list. Cities, regions, countries and areas accepted.'
                 name=''
                 id=''></textarea>
             </div>
           </div>
-          {/* employment */}{" "}
+          <div>
+            <label htmlFor=''>Employment and Industry:</label>
+            <div className='flex justify-center items-center p-2 !border-solid !border-white border-[3px] w-[70%] rounded-xl'>
+              <textarea
+                {...register("employmentSituation")}
+                className='w-full h-full outline-none bg-transparent'
+                placeholder='Enter employment situation as a comma separated list. Categories, industries and specific roles accepted.'
+                name='employment'
+                id=''></textarea>
+            </div>
+          </div>
+          <div>
+            <label htmlFor=''>Household Income:</label>
+            <div className='flex justify-center items-center p-2 !border-solid !border-white border-[3px] w-[70%] rounded-xl'>
+              <select {...register("householdIncome")} className='bg-transparent w-full outline-none'>
+                <option value=' '>Select Household Income:</option>
+                <option value='0 - 19,999'>0 - 19,999</option>
+                <option value='20,000 - 39,999'>20,000 - 39,999</option>
+                <option value='40,000 - 59,999'>40,000 - 59,999</option>
+                <option value='60,000 - 79,999'>60,000 - 79,999</option>
+                <option value='80,000 - 99,999'>80,000 - 99,999</option>
+                <option value='100,000+'>100,000+</option>
+              </select>
+            </div>
+          </div>
+          <div>
+            <label htmlFor=''>Marital Status:</label>
+
+            <div className='flex flex-row w-[70%]'>
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    {...register("maritalStatus")}
+                    value='Single'
+                    defaultChecked
+                    sx={{
+                      color: blue[800],
+                      "&.Mui-checked": {
+                        color: blue[600],
+                      },
+                    }}
+                  />
+                }
+                label='Single'
+              />
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    {...register("maritalStatus")}
+                    value='Married'
+                    defaultChecked
+                    sx={{
+                      color: blue[800],
+                      "&.Mui-checked": {
+                        color: blue[600],
+                      },
+                    }}
+                  />
+                }
+                label='Married'
+              />
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    {...register("maritalStatus")}
+                    value='Widowed'
+                    defaultChecked
+                    sx={{
+                      color: blue[800],
+                      "&.Mui-checked": {
+                        color: blue[600],
+                      },
+                    }}
+                  />
+                }
+                label='Widowed'
+              />
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    {...register("maritalStatus")}
+                    value='Divorced'
+                    defaultChecked
+                    sx={{
+                      color: blue[800],
+                      "&.Mui-checked": {
+                        color: blue[600],
+                      },
+                    }}
+                  />
+                }
+                label='Divorced'
+              />
+            </div>
+          </div>
           <button
             onClick={() => {
               handleSubmit((data) => {
@@ -116,5 +216,7 @@ export default function NewForm() {
           </button>
         </div>
       );
+    case 4:
+      return <div>hello</div>;
   }
 }
