24e5a608bad63588c480627609a63d134795909e
nikhilp1234567 <nikhil_parmar@hotmail.com>
2025-04-01 16:08:04 +0100

    more to-do fixes

--- my-components/NewForm.tsx
+++ my-components/NewForm.tsx
@@ -197,8 +197,39 @@ export default function NewForm() {
                   id=''></textarea>
               </div>
             </div>
-
             <div className='space-y-2'>
+              <label className='text-lg font-medium text-blue-300'>Household Income</label>
+              <div className='flex flex-row gap-6 w-[100%] p-3'>
+                {[
+                  { value: "0 - 19,999", label: "£0-19,999" },
+                  { value: "20,000 - 39,999", label: "£20,000-£39,999" },
+                  { value: "40,000 - 59,999", label: "£40,000-£59,999" },
+                  { value: "60,000 - 79,999", label: "£60,000-£79,999" },
+                  { value: "80,000 - 99,999", label: "£80,000-£99,999" },
+                  { value: "100,000+", label: "£100,000+" },
+                ].map((income) => (
+                  <FormControlLabel
+                    key={income.value}
+                    control={
+                      <Checkbox
+                        {...register("householdIncome")}
+                        value={income.value}
+                        defaultChecked
+                        sx={{
+                          color: blue[800],
+                          "&.Mui-checked": {
+                            color: blue[600],
+                          },
+                        }}
+                      />
+                    }
+                    label={income.label}
+                    className='text-blue-300'
+                  />
+                ))}
+              </div>
+            </div>
+            {/* <div className='space-y-2'>
               <label className='text-lg font-medium text-blue-300'>Household Income</label>
               <div className='flex justify-center items-center p-3 !border-solid !border-blue-900 border-[2px] w-[70%] rounded-xl hover:border-blue-700 transition-colors'>
                 <select {...register("householdIncome")} className='bg-[#070F2B] text-white w-full outline-none cursor-pointer'>
@@ -211,7 +242,7 @@ export default function NewForm() {
                   <option value='100,000+'>$100,000+</option>
                 </select>
               </div>
-            </div>
+            </div> */}
 
             <div className='space-y-2'>
               <label className='text-lg font-medium text-blue-300'>Marital Status</label>
